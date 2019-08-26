6cd5ac45a5cde71c3077597adbc2020a276b2e88
text70 <31259119+text70@users.noreply.github.com>
2019-08-26 17:03:17 -0500

    Add files via upload

--- psg_tokenizer.py
+++ psg_tokenizer.py
@@ -1,21 +1,34 @@
+#! /usr/bin/env python
+
+#######################################
+# psg.py
+#
+# A script to tokenize your space-oriented files  
+# to a single shotgun sequence of tokenized strings
+#
+# Author: <D.lee>
+# Date: 08/26/2019
+# Version: 1.0
+#######################################
+
 from nltk.tokenize import WhitespaceTokenizer, LineTokenizer, TabTokenizer
 from pathlib import Path
 import numpy as np
 
-
-p = Path.home()/'Data'/'PDB'/'2dpg.pdb'
 #Path.home = /home/my_username, thereforth , change the ''Data'/'PDB'/'2dpg.pdb' to your path of interest.
 #use the pathlib read_text to open, read and close the file
+p = Path.home()/'Data'/'PDB'/'2dpg.pdb'
+
 s = p.read_text()
-#Tokenize with Whitespace
+#Tokenize with whitespace
 g = WhitespaceTokenizer().tokenize(s)
-#print(g)
+#Tokenize with lines
 c = LineTokenizer(blanklines='discard').tokenize(s)
-#print(c)
+#Tokenize with tabs
 d = TabTokenizer().tokenize(s)
 
 x = [g, c, d]
 print(np.ma.asarray([x]))
 
 #if __name__ == '__main__':
-    #return()
+    #main()
